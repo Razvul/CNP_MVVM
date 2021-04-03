2be0d06eb9caf1d477ec6a2a0911bb6c9defec12
Razvan Agafitei <razvule@gmail.com>
2021-04-03 14:43:33 +0300

    Installing...

---
+++ CNP_MVVM/ViewModels/ShellViewModels.cs
@@ -0,0 +1,25 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNP_MVVM.ViewModels
+{
+    public class ShellViewModels : ViewModelBase
+    {
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                this.RaisePropertyChanged("FirstName");
+            }
+        }
+
+    }
+}
