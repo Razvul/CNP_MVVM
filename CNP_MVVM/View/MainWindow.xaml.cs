dc14774a1b544ad63879d783f1777c4ec4cd626f
Andrei Vasiliu <andrei@anvaplus.com>
2021-04-03 16:21:43 +0200

    [binding_init] Add first binding

---
+++ CNP_MVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CNP_MVVM.View;
+
+namespace CNP_MVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Person_Test_Click(object sender, RoutedEventArgs e)
+        {
+            var a = new AddPersonWindow();
+            a.ShowDialog();
+        }
+    }
+}
