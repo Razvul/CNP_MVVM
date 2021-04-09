2ad51ecdbb40f3ee0806bc14381925c7baef0577
Razvan Agafitei <razvule@gmail.com>
2021-04-09 10:09:31 +0300

    Binding Combobox Days First implementation

--- CNP_MVVM/ViewModel/CustomDateViewModel.cs
+++ CNP_MVVM/ViewModel/CustomDateViewModel.cs
@@ -31,6 +31,7 @@ namespace CNP_MVVM.ViewModel
             }
         }
 
+        #region Years
         private void GetYear()
         {
             for (int i = 1900; i <= DateTime.Now.Year; i++)
@@ -58,15 +59,63 @@ namespace CNP_MVVM.ViewModel
                 OnPropertyChanged(nameof(YearSource));
             }
         }
+        #endregion
+
+        #region Days
+        private int _selectedItemLuni;
+        public int SelectedItemLuni // primeste selected index de la combobox luni
+        {
+            get
+            {
+                return _selectedItemLuni;
+            }
+            set
+            {
+                _selectedItemLuni = value;
+            }
+        }
+
+        private int GetMaxDays(int selectedIndex) // primeste ca argument SelectedItemLuni
+        {
+            switch (selectedIndex)
+            {
+                case 1: return 29;
+                case 3:
+                case 5:
+                case 8:
+                case 10: return 30;
+                default: return 31;
+            }
+        } // returneaza numarul maxim de zile dintr-o luna
+
+        private void GetDays() //primeste numarul maxim de zile dintr-o luna
+        {
+            for (int i = 1900; i <= GetMaxDays(SelectedItemLuni); i++)
+            {
+                _daySource.Add(i);
+            }
+        } //si umple lista _daySource cu numere de la 1 la GetMaxDays(SelectedItemLuni)
 
         private ObservableCollection<int> _daySource;
 
         public ObservableCollection<int> DaySource
         {
-            get { return _daySource; }
-            set { _daySource = value; }
+            get
+            {
+                if (_daySource == null)//umple lista daca este goala
+                {
+                    _daySource = new ObservableCollection<int>();
+                    GetDays();
+                }
+                return _daySource;
+            }
+            set
+            {
+                _daySource = value;
+                OnPropertyChanged(nameof(_daySource));
+            }
         }
-
+        #endregion
 
         #region INotifyPropertyChanged Implementation
         public event PropertyChangedEventHandler PropertyChanged;
